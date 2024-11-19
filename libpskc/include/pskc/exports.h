e341e27ee4a8f846f40b632f6b71142b8d1cfd29
Simon Josefsson <simon@josefsson.org>
2024-11-19 10:10:27 +0100

    Avoid obsolete FSF postal address.

--- libpskc/include/pskc/exports.h
+++ libpskc/include/pskc/exports.h
@@ -1,5 +1,5 @@
 /*
- * pskc/export.h - library visibility macro.
+ * pskc/exports.h - library visibility macro.
  * Copyright (C) 2012-2024 Simon Josefsson
  *
  * This library is free software; you can redistribute it and/or
@@ -13,9 +13,8 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA
+ * License along with this library; if not, see
+ * <https://www.gnu.org/licenses/>.
  *
  */
 
