ef56e0ddb1925c692ecd857a83dfcc98fa0067a7
Simon Josefsson <simon@josefsson.org>
2026-01-27 14:24:20 +0100

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-old_NEWS_hash = dd89ce6fa4b2679afd19475670b3ffb2
+old_NEWS_hash = 1f325b5789d4190340fcc0bf2713200b
 
 guix = $(shell command -v guix > /dev/null && echo ,guix)
 bootstrap-tools = git,gnulib,autoconf,automake,libtoolize,make,bison,help2man,gengetopt,gtkdocize,tar,gzip$(guix)
